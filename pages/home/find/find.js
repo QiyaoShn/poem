283a3e082ca2be449b2db892a3063225b804235c
QiyaoShn <18272214732@163.com>
2020-07-06 11:18:16 +0800

    间距修改

--- pages/home/find/find.js
+++ pages/home/find/find.js
@@ -1,3 +1,26 @@
+var app=getApp();
+var searchValue='';
 Page({
+  data:{
+    inputShow:true,
+    searchValue:'',
+  },
+  onLoad: function(e){
+  
+  },
+  searchValueInput:function(e){
+    var value=e.detail.value;
+    this.setData({
+      searchValue:value,
+    })
+    console.log(searchValue);
+  },
+  sousuoId:function(e){
+    var ID=event.currentTarget.dataset.ID
+    console.log(ID)
+    wx.navigateTo({
+      url: '/pages/home/content/detail/detail',
+    })
+  }
   
 })
