0dda7ffff6b51977f00c2031d2048c2243ced9f4
silent355 <1609966480@qq.com>
2021-05-20 13:13:06 +0800

    视频

--- miniprogramRoot/pages/home/content/shibie/shibie.js
+++ miniprogramRoot/pages/home/content/shibie/shibie.js
@@ -12,38 +12,38 @@ Page({
   data: {
     //语音
     recordState: false, //录音状态
-    content: '',//内容
+    content: '', //内容
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     //识别语音
     this.initRecord();
   },
   // 手动输入内容
-  conInput: function (e) {
+  conInput: function(e) {
     this.setData({
       content: e.detail.value,
     })
   },
   //识别语音 -- 初始化
-  initRecord: function () {
+  initRecord: function() {
     const that = this;
     // 有新的识别内容返回，则会调用此事件
-    manager.onRecognize = function (res) {
+    manager.onRecognize = function(res) {
       console.log(res)
     }
     // 正常开始录音识别时会调用此事件
-    manager.onStart = function (res) {
+    manager.onStart = function(res) {
       console.log("成功开始录音识别", res)
     }
     // 识别错误事件
-    manager.onError = function (res) {
+    manager.onError = function(res) {
       console.error("error msg", res)
     }
     //识别结束事件
-    manager.onStop = function (res) {
+    manager.onStop = function(res) {
       console.log('..............结束录音')
       console.log('录音临时文件地址 -->' + res.tempFilePath);
       console.log('录音总时长 -->' + res.duration + 'ms');
@@ -54,7 +54,7 @@ Page({
           title: '提示',
           content: '听不清楚，请重新说一遍！',
           showCancel: false,
-          success: function (res) { }
+          success: function(res) {}
         })
         return;
       }
@@ -65,17 +65,17 @@ Page({
     }
   },
   //语音  --按住说话
-  touchStart: function (e) {
+  touchStart: function(e) {
     this.setData({
-      recordState: true  //录音状态
+      recordState: true //录音状态
     })
     // 语音开始识别
     manager.start({
-      lang: 'zh_CN',// 识别的语言，目前支持zh_CN en_US zh_HK sichuanhua
+      lang: 'zh_CN', // 识别的语言，目前支持zh_CN en_US zh_HK sichuanhua
     })
   },
   //语音  --松开结束
-  touchEnd: function (e) {
+  touchEnd: function(e) {
     this.setData({
       recordState: false
     })
